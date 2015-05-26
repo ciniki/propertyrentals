34174c13c514ff4c850e4937fcbc7111b986bcfa
Andrew Rivett <veggiefrog@gmail.com>
2015-05-26 14:31:30 -0400

    Added tables and private code

---
+++ private/maps.php
@@ -0,0 +1,30 @@
+<?php
+//
+// Description
+// -----------
+// The module flags
+//
+// Arguments
+// ---------
+//
+// Returns
+// -------
+//
+function ciniki_propertyrentals_maps($ciniki, $modules) {
+	$maps = array();
+	$maps['property'] = array(
+		'status'=>array(
+			'10'=>'Available',
+			'20'=>'Rented',
+			'50'=>'Archived',
+			'60'=>'Deleted',
+			),
+		'flags'=>array(
+			0x01=>'Public',
+			0x02=>'Private',
+			),
+		);
+
+	return array('stat'=>'ok', 'maps'=>$maps);
+}
+?>
